55bfaaab72314b3a57d07f6cfe684f9f757acf25
Leo Zurbriggen <leo.zurbriggen@gmail.com>
2021-10-17 17:56:27 +0200

    basic jump mechanic, some sprites, cleanup

--- player/Player.cs
+++ player/Player.cs
@@ -1,30 +1,57 @@
 using Godot;
 using System;
 
-public class Player : Node {
-  // Declare member variables here. Examples:
-  // private int a = 2;
-  // private string b = "text";
-
-  private PhysicsBody2D body;
-  private PhysicsBody2D hand;
+public class Player : Node2D {
+  private RigidBody2D body;
+  private StaticBody2D hand;
   private Line2D armLine;
+  private DampedSpringJoint2D armSpring;
+
+  private bool closed = false;
+  private Vector2 closePosition = Vector2.Zero;
 
   public override void _Ready() {
-    body = GetNode<PhysicsBody2D>("body");
-    hand = GetNode<PhysicsBody2D>("hand");
+    body = GetNode<RigidBody2D>("body");
+    hand = GetNode<StaticBody2D>("hand");
     armLine = GetNode<Line2D>("armLine");
+    armSpring = GetNode<DampedSpringJoint2D>("armSpring");
+
+    // TODO: move to appropriate script
+    Input.SetMouseMode(Input.MouseMode.Confined);
+
   }
 
   public override void _PhysicsProcess(float delta) {
+    if (closed) {
+      hand.GlobalPosition = closePosition;
+    } else {
+      hand.GlobalPosition = body.GlobalPosition + body.GlobalPosition.DirectionTo(GetGlobalMousePosition()) * Mathf.Min(body.GlobalPosition.DistanceTo(GetGlobalMousePosition()), 48);
+    }
     armLine.SetPointPosition(0, body.Position);
     armLine.SetPointPosition(1, hand.Position);
     armLine.Width = (1f - Mathf.Clamp(body.Position.DistanceTo(hand.Position) / 48f, 0f, 1f)) * 4f + 2f;
   }
 
   public override void _Input(InputEvent ev) {
-    if (ev is InputEventMouseMotion) {
-      hand.Position = (ev as InputEventMouseMotion).Position;
+
+    if (Input.IsActionJustPressed("grab")) {
+      closed = true;
+      armSpring.NodeA = body.GetPath();
+      armSpring.NodeB = hand.GetPath();
+      closePosition = GetGlobalMousePosition();
+    }
+
+    if (closed && Input.IsActionJustReleased("ui_accept")) {
+      armSpring.NodeA = "";
+      armSpring.NodeB = "";
+      closed = false;
+      // TODO: calculate fractional impulse
+      body.ApplyImpulse(Vector2.Zero, body.Position.DirectionTo(hand.Position) * 500f);
+    }
+
+    // TODO: move to appropriate script
+    if (Input.IsActionPressed("ui_cancel")) {
+      GetTree().Quit();
     }
   }
 }
---
+++ player/PlayerBody.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class PlayerBody : Node2D {
+
+  public override void _Ready() {
+
+  }
+
+  public override void _PhysicsProcess(float delta) {
+    GlobalRotation = 0;
+  }
+}
